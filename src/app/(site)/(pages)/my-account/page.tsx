105e70cdfb6c11eab58f52601e0e2e1fe19cbefe
Jorge Passetti <jorgepassetti@jorges-MBP.(none)>
2025-09-05 18:01:53 +0200

    Firs changes

--- src/app/(site)/(pages)/my-account/page.tsx
+++ src/app/(site)/(pages)/my-account/page.tsx
@@ -1,10 +1,10 @@
-import MyAccount from "@/components/MyAccount";
-import React from "react";
+import MyAccount from '@/components/MyAccount';
+import React from 'react';
 
-import { Metadata } from "next";
+import { Metadata } from 'next';
 export const metadata: Metadata = {
-  title: "My Account | NextCommerce Nextjs E-commerce template",
-  description: "This is My Account page for NextCommerce Template",
+  title: 'My Cuenta | NextCommerce Nextjs E-commerce template',
+  description: 'This is My Cuenta page for NextCommerce Template',
   // other metadata
 };
 
